e8557b2ab28eae708c37720e16a8f472cea1a96b
baptisteghostine <ghostine.baptiste@gmail.com>
2024-10-07 13:19:00 +0200

    style(ui): improve text design with gradient colors and layout adjustments

--- app/src/main/java/com/github/se/orator/MainActivity.kt
+++ app/src/main/java/com/github/se/orator/MainActivity.kt
@@ -6,19 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.se.orator.ui.authentification.SignInScreenTest
+import com.github.se.orator.ui.authentification.SignInScreen
 import com.github.se.orator.ui.theme.ProjectTheme
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : ComponentActivity() {
   private lateinit var auth: FirebaseAuth
-
 
   @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +31,9 @@ class MainActivity : ComponentActivity() {
     }
 
     enableEdgeToEdge()
-    setContent {
-      ProjectTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { SignInScreenTest() } }
-    }
+    setContent { ProjectTheme { Scaffold(modifier = Modifier.fillMaxSize()) { SignInScreen() } } }
   }
 }
-
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
