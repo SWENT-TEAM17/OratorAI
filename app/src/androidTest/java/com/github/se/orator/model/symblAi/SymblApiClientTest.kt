796ee7868b6782bade97599935b53105721f5070
Mathis Hage <mathis.hage@epfl.ch>
2024-11-21 21:59:22 +0100

    test: add some tests for the new analytics in `SymblApiClient.kt`

--- app/src/androidTest/java/com/github/se/orator/model/symblAi/SymblApiClientTest.kt
+++ app/src/androidTest/java/com/github/se/orator/model/symblAi/SymblApiClientTest.kt
@@ -6,6 +6,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.github.se.orator.model.speaking.AnalysisData
 import java.io.File
+import java.io.IOException
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import okhttp3.Call
@@ -15,6 +16,7 @@ import okhttp3.Protocol
 import okhttp3.Request
 import okhttp3.Response
 import okhttp3.ResponseBody
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -22,6 +24,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import org.mockito.kotlin.any
+import org.mockito.kotlin.verify
 
 @RunWith(AndroidJUnit4::class)
 class SymblApiClientTest {
@@ -30,11 +33,60 @@ class SymblApiClientTest {
   private lateinit var symblApiClient: SymblApiClient
   private lateinit var okHttpClient: OkHttpClient
 
+  private lateinit var callAccessToken: Call
+  private lateinit var callFailure: Call
+  private lateinit var callJobStatus: Call
+  private lateinit var callSendFile: Call
+  private lateinit var callTranscription: Call
+  private lateinit var callAnalyticsInvalidSize: Call
+  private lateinit var callAnalyticsInvalidContent: Call
+
+  private val responseAccessToken = makeResponse(200, "OK", "{\"accessToken\":\"test\"}")
+  private val responseFailure = makeResponse(500, "Internal Server Error", "")
+  private val responseJobStatus = makeResponse(200, "OK", "{\"status\":\"completed\"}")
+  private val responseSendFile =
+      makeResponse(200, "OK", "{\"conversationId\":\"0\", \"jobId\":\"0\"}")
+  private val responseTranscription =
+      makeResponse(
+          200,
+          "OK",
+          "{\"messages\": [{\"text\": \"test text\", \"sentiment\": {\"polarity\": {\"score\": 0.5}, \"suggested\": \"neutral\"}}]}")
+  private val responseAnalyticsInvalidArraySize = makeResponse(200, "OK", "{\"members\": []}")
+  private val responseAnalyticsInvalidContent = makeResponse(200, "OK", "{\"members\": [{}]}")
+
   @Before
   fun setUp() {
     // Obtain the application context
     context = ApplicationProvider.getApplicationContext()
     okHttpClient = mock(OkHttpClient::class.java)
+    callAccessToken = mock(Call::class.java)
+    callFailure = mock(Call::class.java)
+    callJobStatus = mock(Call::class.java)
+    callSendFile = mock(Call::class.java)
+    callTranscription = mock(Call::class.java)
+    callAnalyticsInvalidSize = mock(Call::class.java)
+    callAnalyticsInvalidContent = mock(Call::class.java)
+
+    `when`(callAccessToken.execute()).thenReturn(responseAccessToken)
+
+    `when`(callFailure.execute()).thenReturn(responseFailure)
+    `when`(callFailure.enqueue(any())).then {
+      val callback = it.arguments[0] as okhttp3.Callback
+      callback.onFailure(callFailure, IOException("Test exception"))
+    }
+
+    `when`(callJobStatus.execute()).thenReturn(responseJobStatus)
+
+    `when`(callSendFile.enqueue(any())).then {
+      val callback = it.arguments[0] as okhttp3.Callback
+      callback.onResponse(callSendFile, responseSendFile)
+    }
+
+    `when`(callTranscription.execute()).thenReturn(responseTranscription)
+
+    `when`(callAnalyticsInvalidSize.execute()).thenReturn(responseAnalyticsInvalidArraySize)
+
+    `when`(callAnalyticsInvalidContent.execute()).thenReturn(responseAnalyticsInvalidContent)
 
     // Initialize SymblApiClient with the context
     symblApiClient = SymblApiClient(context)
@@ -151,7 +203,7 @@ class SymblApiClientTest {
   }
 
   @Test
-  fun messageParsingFailureCallsOnError() {
+  fun tokenParsingFailureCallsOnError() {
     symblApiClient = SymblApiClient(context, okHttpClient)
 
     val call = mock(Call::class.java)
@@ -169,10 +221,12 @@ class SymblApiClientTest {
 
     `when`(okHttpClient.newCall(any())).thenReturn(call)
 
+    var retrieval: SpeakingError? = null
+
     symblApiClient.getTranscription(
-        file,
-        { Assert.fail("Should not have succeeded") },
-        { Assert.assertTrue(it == SpeakingError.JSON_PARSING_ERROR) })
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    Assert.assertTrue(retrieval == SpeakingError.JSON_PARSING_ERROR)
   }
 
   @Test
@@ -180,43 +234,162 @@ class SymblApiClientTest {
     symblApiClient = SymblApiClient(context, okHttpClient)
 
     val file = mock(File::class.java)
-    val callAccessToken = mock(Call::class.java)
-    val callTranscription = mock(Call::class.java)
-
-    `when`(callAccessToken.execute())
-        .thenReturn(
-            Response.Builder()
-                .request(Request.Builder().url("http://localhost").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(200)
-                .message("OK")
-                .body(
-                    ResponseBody.create(
-                        "application/json".toMediaType(), "{\"accessToken\":\"test\"}"))
-                .build())
-
-    `when`(callTranscription.execute())
-        .thenReturn(
-            Response.Builder()
-                .request(Request.Builder().url("http://localhost").build())
-                .protocol(Protocol.HTTP_1_1)
-                .code(500)
-                .message("Internal Server Error")
-                .body(ResponseBody.create("application/json".toMediaType(), ""))
-                .build())
 
     `when`(okHttpClient.newCall(any())).then {
       val request = it.arguments[0] as Request
       if (request.url.toString().contains("oauth2/token")) {
         callAccessToken
       } else {
-        callTranscription
+        callFailure
       }
     }
 
+    var retrieval: SpeakingError? = null
+
     symblApiClient.getTranscription(
-        file,
-        { Assert.fail("Should not have succeeded") },
-        { Assert.assertTrue(it == SpeakingError.HTTP_REQUEST_ERROR) })
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    // Verify that the call to send the file is made
+    verify(callFailure).enqueue(any())
+
+    Assert.assertTrue(retrieval == SpeakingError.HTTP_REQUEST_ERROR)
+  }
+
+  @Test
+  fun transcriptionAndSentimentFailToRetrieveCallsOnFailure() {
+    symblApiClient = SymblApiClient(context, okHttpClient)
+
+    val file = mock(File::class.java)
+
+    `when`(okHttpClient.newCall(any())).then {
+      val request = it.arguments[0] as Request
+      if (request.url.toString().contains("oauth2/token")) {
+        callAccessToken
+      } else if (request.url.toString().contains("process")) {
+        callSendFile
+      } else if (request.url.toString().contains("job")) {
+        callJobStatus
+      } else {
+        callFailure
+      }
+    }
+
+    var retrieval: SpeakingError? = null
+
+    symblApiClient.getTranscription(
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    // Verify that the call to retrieve the transcription is made
+    verify(callFailure).execute()
+
+    Assert.assertTrue(retrieval == SpeakingError.HTTP_REQUEST_ERROR)
+  }
+
+  @Test
+  fun transcriptionAndAnalyticsFailToRetrieveCallsOnFailure() {
+    symblApiClient = SymblApiClient(context, okHttpClient)
+
+    val file = mock(File::class.java)
+
+    `when`(okHttpClient.newCall(any())).then {
+      val request = it.arguments[0] as Request
+      if (request.url.toString().contains("oauth2/token")) {
+        callAccessToken
+      } else if (request.url.toString().contains("process")) {
+        callSendFile
+      } else if (request.url.toString().contains("job")) {
+        callJobStatus
+      } else if (request.url.toString().contains("messages")) {
+        callTranscription
+      } else {
+        callFailure
+      }
+    }
+
+    var retrieval: SpeakingError? = null
+
+    symblApiClient.getTranscription(
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    // Verify that the calls to retrieve the transcription and then the analysis fields
+    // were indeed made
+    verify(callTranscription).execute()
+    verify(callFailure).execute()
+
+    Assert.assertTrue(retrieval == SpeakingError.HTTP_REQUEST_ERROR)
+  }
+
+  @Test
+  fun analyticsArrayInvalidCallsOnFailure() {
+    symblApiClient = SymblApiClient(context, okHttpClient)
+
+    val file = mock(File::class.java)
+
+    `when`(okHttpClient.newCall(any())).then {
+      val request = it.arguments[0] as Request
+      if (request.url.toString().contains("oauth2/token")) {
+        callAccessToken
+      } else if (request.url.toString().contains("process")) {
+        callSendFile
+      } else if (request.url.toString().contains("job")) {
+        callJobStatus
+      } else if (request.url.toString().contains("messages")) {
+        callTranscription
+      } else {
+        callAnalyticsInvalidSize
+      }
+    }
+
+    var retrieval: SpeakingError? = null
+
+    symblApiClient.getTranscription(
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    // Verify that the call to retrieve the analytics was made
+    verify(callAnalyticsInvalidSize).execute()
+
+    Assert.assertTrue(retrieval == SpeakingError.NO_ANALYTICS_FOUND_ERROR)
+  }
+
+  @Test
+  fun analyticsArrayInvalidContentCallsOnFailure() {
+    symblApiClient = SymblApiClient(context, okHttpClient)
+
+    val file = mock(File::class.java)
+
+    `when`(okHttpClient.newCall(any())).then {
+      val request = it.arguments[0] as Request
+      if (request.url.toString().contains("oauth2/token")) {
+        callAccessToken
+      } else if (request.url.toString().contains("process")) {
+        callSendFile
+      } else if (request.url.toString().contains("job")) {
+        callJobStatus
+      } else if (request.url.toString().contains("messages")) {
+        callTranscription
+      } else {
+        callAnalyticsInvalidContent
+      }
+    }
+
+    var retrieval: SpeakingError? = null
+
+    symblApiClient.getTranscription(
+        file, { Assert.fail("Should not have succeeded") }, { retrieval = it })
+
+    // Verify that the call to retrieve the analytics was made
+    verify(callAnalyticsInvalidContent).execute()
+
+    Assert.assertTrue(retrieval == SpeakingError.JSON_PARSING_ERROR)
+  }
+
+  private fun makeResponse(code: Int, message: String, body: String): Response {
+    return Response.Builder()
+        .request(Request.Builder().url("http://localhost").build())
+        .protocol(Protocol.HTTP_1_1)
+        .code(code)
+        .message(message)
+        .body(body.toResponseBody("application/json".toMediaType()))
+        .build()
   }
 }
