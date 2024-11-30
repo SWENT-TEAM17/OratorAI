3c6373a2f65bdc73c78a1de3c41c3cba1c85c943
Mathis Hage <mathis.hage@epfl.ch>
2024-12-01 00:54:49 +0100

    fix: add colors link for `BottomNavigationMenu.kt`

--- app/src/main/java/com/github/se/orator/ui/navigation/BottomNavigationMenu.kt
+++ app/src/main/java/com/github/se/orator/ui/navigation/BottomNavigationMenu.kt
@@ -38,12 +38,12 @@ fun BottomNavigationMenu(
           BottomNavigationItem(
               icon = {
                 if (tab.route == selectedItem) {
-                  Icon(tab.coloredIcon, contentDescription = null)
+                  Icon(tab.coloredIcon, contentDescription = null, tint = MaterialTheme.colorScheme.primary)
                 } else {
                   Icon(tab.outlinedIcon, contentDescription = null)
                 }
               },
-              label = { Text(tab.textId) },
+              label = { Text(tab.textId, color = MaterialTheme.colorScheme.onSurface) },
               selected = tab.route == selectedItem,
               onClick = { onTabSelect(tab) },
               modifier = Modifier.clip(AppShapes.bottomNavigationItemShape).testTag(tab.textId))
