88dd594222fbee5e761fe6fae1d2094a6cdd66c9
Mathis Hage <mathis.hage@epfl.ch>
2024-12-01 13:23:56 +0100

    refactor: fix the imports in `FriendsScreen.kt`
    Following the recommendation of @helkhour regarding the material/material3
    imports.

--- app/src/main/java/com/github/se/orator/ui/friends/FriendsScreen.kt
+++ app/src/main/java/com/github/se/orator/ui/friends/FriendsScreen.kt
@@ -11,13 +11,28 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Whatshot
-import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -160,12 +175,12 @@ fun ViewFriendsScreen(
                       Box(
                           modifier =
                               Modifier.padding(vertical = AppDimensions.paddingMediumSmall)) {
-                            androidx.compose.material.OutlinedTextField(
+                            OutlinedTextField(
                                 value = searchQuery,
                                 onValueChange = { searchQuery = it },
-                                label = { androidx.compose.material.Text("Search for a friend.") },
+                                label = { Text("Search for a friend.") },
                                 leadingIcon = {
-                                  androidx.compose.material.Icon(
+                                  Icon(
                                       imageVector = Icons.Default.Search,
                                       contentDescription = "Search Icon")
                                 },
