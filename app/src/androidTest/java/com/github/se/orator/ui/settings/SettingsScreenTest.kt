d5dde69fa584887ed376f58040979ae91e843e5d
Mathis Hage <mathis.hage@epfl.ch>
2024-12-04 22:29:01 +0100

    test: add tests for the in-app theme swap
    Both unit and UI tests.

--- app/src/androidTest/java/com/github/se/orator/ui/settings/SettingsScreenTest.kt
+++ app/src/androidTest/java/com/github/se/orator/ui/settings/SettingsScreenTest.kt
@@ -1,9 +1,11 @@
 package com.github.se.orator.ui.settings
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.github.se.orator.model.profile.UserProfileRepository
 import com.github.se.orator.model.profile.UserProfileViewModel
+import com.github.se.orator.model.theme.AppThemeViewModel
 import com.github.se.orator.ui.navigation.NavigationActions
 import com.github.se.orator.ui.navigation.Screen
 import org.junit.Before
@@ -26,6 +28,7 @@ class SettingsScreenTest {
     navigationActions = mock(NavigationActions::class.java)
     userProfileRepository = mock(UserProfileRepository::class.java)
     userProfileViewModel = UserProfileViewModel(userProfileRepository)
+
     `when`(navigationActions.currentRoute()).thenReturn(Screen.HOME)
   }
 
@@ -64,5 +67,23 @@ class SettingsScreenTest {
       composeTestRule.onNodeWithTag(tag).assertExists()
       composeTestRule.onNodeWithTag(tag).performClick()
     }
+  }
+
+  @Test
+  fun clickOnThemeButton() {
+    var appThemeViewModel: AppThemeViewModel? = null
+
+    composeTestRule.setContent {
+      appThemeViewModel = AppThemeViewModel(LocalContext.current)
+      appThemeViewModel?.saveTheme(false)
+      SettingsScreen(
+          navigationActions = navigationActions,
+          userProfileViewModel = userProfileViewModel,
+          themeViewModel = appThemeViewModel)
+    }
+
+    assert(appThemeViewModel?.isDark?.value == false)
+    composeTestRule.onNodeWithTag("theme").performClick()
+    assert(appThemeViewModel?.isDark?.value == true)
   }
 }
