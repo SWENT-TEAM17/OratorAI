75518dc26a1f78ba23de08d3bc4979e47198783e
Iskandar Ghobril <ghobriliskandar@gmail.com>
2024-12-10 16:55:12 +0100

    ktfmt format

--- app/src/main/java/com/github/se/orator/ui/offline/RecordingReviewScreen.kt
+++ app/src/main/java/com/github/se/orator/ui/offline/RecordingReviewScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
