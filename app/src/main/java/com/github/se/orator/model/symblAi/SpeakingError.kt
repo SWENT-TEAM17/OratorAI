a0376501be3719caeef1736e736815bd0256c2a9
Mathis Hage <mathis.hage@epfl.ch>
2024-12-18 19:50:46 +0100

    refactor: modularize the error strings in `SpeakingError.kt`

--- app/src/main/java/com/github/se/orator/model/symblAi/SpeakingError.kt
+++ app/src/main/java/com/github/se/orator/model/symblAi/SpeakingError.kt
@@ -1,5 +1,9 @@
 package com.github.se.orator.model.symblAi
 
+
+/**
+ * Enum class to represent the different errors that can occur during the processing of the user's speech.
+ */
 enum class SpeakingError {
   NO_ERROR,
   CREDENTIALS_ERROR,
@@ -14,14 +18,21 @@ enum class SpeakingError {
   override fun toString(): String {
     return when (this) {
       NO_ERROR -> "No error"
-      CREDENTIALS_ERROR -> "There was an error with the SymblAI data"
-      ACCESS_TOKEN_ERROR -> "There was an error with the SymblAI data"
-      HTTP_REQUEST_ERROR -> "There was an error when issuing the HTTP request"
-      JOB_PROCESSING_ERROR -> "There was an error processing the job"
-      MISSING_CONV_ID_ERROR -> "There was an error with the SymblAI data"
-      JSON_PARSING_ERROR -> "There was an error processing the received data from SymblAI"
-      NO_MESSAGES_FOUND_ERROR -> "There was an error processing the received data from SymblAI"
-      NO_ANALYTICS_FOUND_ERROR -> "There was an error processing the received data from SymblAI"
+      CREDENTIALS_ERROR -> Companion.STRING_SYMBL_DATA_ERROR
+      ACCESS_TOKEN_ERROR -> Companion.STRING_SYMBL_DATA_ERROR
+      HTTP_REQUEST_ERROR -> Companion.STRING_HTTP_ERROR
+      JOB_PROCESSING_ERROR -> Companion.STRING_SYMBL_JOB_ERROR
+      MISSING_CONV_ID_ERROR -> Companion.STRING_SYMBL_DATA_ERROR
+      JSON_PARSING_ERROR -> Companion.STRING_SYMBL_PROCESSING_ERROR
+      NO_MESSAGES_FOUND_ERROR -> Companion.STRING_SYMBL_PROCESSING_ERROR
+      NO_ANALYTICS_FOUND_ERROR -> Companion.STRING_SYMBL_PROCESSING_ERROR
     }
+  }
+
+  companion object {
+    const val STRING_HTTP_ERROR = "There was an error when issuing the HTTP request"
+    const val STRING_SYMBL_DATA_ERROR = "There was an error with the SymblAI data"
+    const val STRING_SYMBL_JOB_ERROR = "There was an error processing the job"
+    const val STRING_SYMBL_PROCESSING_ERROR = "There was an error processing the data from SymblAI"
   }
 }
