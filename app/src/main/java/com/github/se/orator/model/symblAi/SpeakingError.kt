02d80a9c5a18f58e81b9fbdffc740b65b0153790
Mathis Hage <mathis.hage@epfl.ch>
2024-12-18 18:38:33 +0100

    fix: override `SymblError` `toString` method for more details on errors
    Also removed the redundant "You said :" before each sentence of the
    transcribed text.

--- app/src/main/java/com/github/se/orator/model/symblAi/SpeakingError.kt
+++ app/src/main/java/com/github/se/orator/model/symblAi/SpeakingError.kt
@@ -9,5 +9,19 @@ enum class SpeakingError {
   MISSING_CONV_ID_ERROR,
   JSON_PARSING_ERROR,
   NO_MESSAGES_FOUND_ERROR,
-  NO_ANALYTICS_FOUND_ERROR
+  NO_ANALYTICS_FOUND_ERROR;
+
+  override fun toString(): String {
+    return when (this) {
+      NO_ERROR -> "No error"
+      CREDENTIALS_ERROR -> "There was an error with the SymblAI data"
+      ACCESS_TOKEN_ERROR -> "There was an error with the SymblAI data"
+      HTTP_REQUEST_ERROR -> "There was an error when issuing the HTTP request"
+      JOB_PROCESSING_ERROR -> "There was an error processing the job"
+      MISSING_CONV_ID_ERROR -> "There was an error with the SymblAI data"
+      JSON_PARSING_ERROR -> "There was an error processing the received data from SymblAI"
+      NO_MESSAGES_FOUND_ERROR -> "There was an error processing the received data from SymblAI"
+      NO_ANALYTICS_FOUND_ERROR -> "There was an error processing the received data from SymblAI"
+    }
+  }
 }
